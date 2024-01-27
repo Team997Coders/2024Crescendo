8c8d8e86a39a6356975492de7c234b3e8b84eed6
Alex_He <118498365+Yuxuan1103@users.noreply.github.com>
2024-01-26 19:47:37 -0800

    change the shooter

--- 2024Crescendo/src/main/java/frc/robot/subsystems/Shooter.java
+++ 2024Crescendo/src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,28 +1,48 @@
 package frc.robot.subsystems;
-
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
 
+
+
 public class Shooter implements Subsystem{
 
-    private final CANSparkMax flywheelNEO = new CANSparkMax(Constants.Shooter.FLYWHEEL_MOTOR_ID, MotorType.kBrushless);
+    private CANSparkMax flywheelNEO = new CANSparkMax(Constants.Shooter.FLYWHEEL_MOTOR_ID, MotorType.kBrushless);  
+    private CANSparkMax invertedFlywheelNEO = new CANSparkMax(Constants.Shooter.FLYWHEEL_MOTOR_ID, MotorType.kBrushless);
+    private final RelativeEncoder ShooterEncoder;
+
+
+    private Shooter() {
+        invertedFlywheelNEO.setInverted(true);
+        invertedFlywheelNEO.follow(flywheelNEO);
+
+        ShooterEncoder = flywheelNEO.getEncoder(); 
+    }
+    
 
     public void setMotorVoltage(double voltage) {
         flywheelNEO.setVoltage(voltage);
+        
     }
 
     public void setMotorOutput(double output) {
         flywheelNEO.set(output); //between -1.0 and 1.0
     }
 
+    public double getFlywheelEncoderPosition() {
+        return ShooterEncoder.getPosition();
+    }
+    public double getFlywheelEncoderVelocity(){
+        return ShooterEncoder.getVelocity();
+    }
+    
     
 
     @Override 
-    public void periodic() {
+    public void periodic() { 
         //loggers eventually
         SmartDashboard.putNumber("", 0); 
         SmartDashboard.putNumber("", 0);
