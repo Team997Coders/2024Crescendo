85db18b89057cae4af06bc924127721d34646d1c
Alex_He <118498365+Yuxuan1103@users.noreply.github.com>
2024-02-02 21:49:13 -0800

    almost done. Need to fix the loop on shooter

--- 2024Crescendo/src/main/java/frc/robot/commands/Shoot.java
+++ 2024Crescendo/src/main/java/frc/robot/commands/Shoot.java
@@ -2,43 +2,54 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-
-import frc.robot.subsystems.IndexerSubsystem;   
+import frc.robot.subsystems.IndexerSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class Shoot extends Command{
+    // define private fields
     private Timer timer;
-    private ShooterSubsystem flywheel;
-    private IndexerSubsystem pushNotes;
-    private double feederVoltage=2;
+    private ShooterSubsystem m_shooter;
+    private IndexerSubsystem m_indexer;
 
-
-
-    public Shoot(ShooterSubsystem flwyheel, IndexerSubsystem pushNotes) {
-        this.pushNotes = pushNotes;
-        this.flywheel = flywheel;
-        this.execute();
+    // constructor
+    public Shoot (ShooterSubsystem shooterStart, IndexerSubsystem indexer){
+        this.m_shooter = shooterStart;
+        this.m_indexer = indexer;
         
     }
 
+
+
     @Override
     public void initialize() {
+        timer = new Timer();
     }
     
-    @Override 
+    @Override
+    /**
+     * Shoot the note.
+     * If we don't have a note loaded, then this command does nothing (just exit)
+     * If we do have a note then:
+     *      start up the shooter to a given speed (voltage)
+     *      when wheels are up to speed (either via a pid, an encoder or just a wait)
+     *      start the indexer (to push the note into the shooter)
+     *      wait for the note sensor to show no more note
+     *      turn off the indexer
+     *      wait 2 seconds (approximately)
+     *      turn off the shooter
+     */
     public void execute() {
-        while (pushNotes.isFilled){
-            if (flywheel.isShooterButtonPressed() == true){
-                timer.start();    
-                if (timer.get() > 3){
-                    pushNotes.setFeederVoltage(feederVoltage);
-                    pushNotes.isFilled();
+        while (m_indexer.isFilled()){
+            if (m_shooter.isShooterButtonPressed() == true){
+                timer.start();
+                if (timer.get() < 5){
                     timer.reset();
                 }
             }else{
-                flywheel.setRightMotorVoltage(0);
+                m_shooter.setLeftMotorVoltage(0);
+                m_shooter.setRightMotorVoltage(0);
             }
-            if(!pushNotes.isFilled){
+            if(!m_indexer.isFilled()){
                 break;
             }
         }
