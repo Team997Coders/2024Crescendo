7e5d8239156489848e20462ab72147d1554a3ae8
Floyd Moore <floyd.moore@gmail.com>
2024-02-04 22:21:31 -0800

    cleanup changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,9 +18,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -33,59 +36,64 @@ public class RobotContainer {
   private final Climb climbDown = new Climb(m_ClimberSubsystem, -2);
   private final Climb dontClimb = new Climb(m_ClimberSubsystem, 0);
 
-
-
-
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private final CommandXboxController m_driverController = new CommandXboxController(
+      OperatorConstants.kDriverControllerPort);
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the trigger bindings
-  
+
     configureBindings();
     populateDashboard();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //new Trigger(m_exampleSubsystem::exampleCondition)
-    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
-    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
     m_driverController.b().whileTrue(new Shoot(m_shooterSubsystem, m_indexerSubsystem, 2, true));
-    //m_driverController.b().onTrue(new Shoot(m_shooterSubsystem, m_indexerSubsystem, 2 ,m_driverController.b().getAsBoolean()));
+    // m_driverController.b().onTrue(new Shoot(m_shooterSubsystem,
+    // m_indexerSubsystem, 2 ,m_driverController.b().getAsBoolean()));
     m_driverController.a().onTrue(new Index(m_indexerSubsystem));
 
-  //  m_driverController.rightBumper().whileTrue(new Climb(m_ClimberSubsystem, -3));
-   // m_driverController.leftBumper().whileTrue(new Climb(m_ClimberSubsystem, 3));
-
- 
+    // m_driverController.rightBumper().whileTrue(new Climb(m_ClimberSubsystem,
+    // -3));
+    // m_driverController.leftBumper().whileTrue(new Climb(m_ClimberSubsystem, 3));
 
     if (m_ClimberSubsystem.getEncoderRotations() < 5) {
       m_driverController.rightBumper().whileTrue(new Climb(m_ClimberSubsystem, -3));
-    }  
+    }
 
-     if (true) {
+    if (true) {
       m_driverController.leftBumper().whileTrue(new Climb(m_ClimberSubsystem, 3));
-    }  
+    }
 
     m_driverController.rightBumper().onFalse(dontClimb);
     m_driverController.leftBumper().onFalse(dontClimb);
 
   }
-
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -96,9 +104,6 @@ public class RobotContainer {
     // An example command will be run in autonomous
     return Autos.exampleAuto(m_indexerSubsystem, m_shooterSubsystem);
   }
-  // public Command getTeleoperateCommand(){
-    
-  // }
 
   public void populateDashboard() {
     SmartDashboard.putData("IndexerSubsystem", m_indexerSubsystem);
@@ -106,6 +111,6 @@ public class RobotContainer {
     SmartDashboard.putBoolean("Note Sensor", m_indexerSubsystem.getSensorStatus());
     SmartDashboard.putNumber("Intake Encoder Position", m_indexerSubsystem.getIntakeEncoderPosition());
     SmartDashboard.putNumber("Shooter Speed", m_shooterSubsystem.getLeftFlywheelEncoderVelocity());
-  } 
+  }
 
 }
