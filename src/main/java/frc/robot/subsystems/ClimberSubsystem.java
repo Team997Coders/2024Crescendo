60697730ffc7b3aeb294b209fc64c1747a7f62f4
Floyd E Moore <floyd.moore@hp.com>
2024-02-07 15:59:24 -0800

    mentor update to climber;

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -10,18 +11,26 @@ import com.revrobotics.CANSparkBase.IdleMode;
 
 public class ClimberSubsystem extends SubsystemBase {
 
-    private final CANSparkMax climberMotor = new CANSparkMax(Constants.Climber.CLIMBER_MOTOR_ID, MotorType.kBrushless);
+    private final CANSparkMax climberMotor;
+    private final RelativeEncoder climberEncoder;
 
     public ClimberSubsystem() {
+        climberMotor = new CANSparkMax(Constants.Climber.CLIMBER_MOTOR_ID, MotorType.kBrushless);
+        climberMotor.restoreFactoryDefaults();
         climberMotor.setIdleMode(IdleMode.kBrake);
         climberMotor.setInverted(Constants.Climber.CLIMBER_MOTOR_IS_INVERTED);
+        climberEncoder = climberMotor.getEncoder();
     }
 
     public void setMotorVoltage(double voltage) {
         climberMotor.setVoltage(voltage);
     }
 
+    public void setMotorOutput(double ratio) {
+        climberMotor.setVoltage(ratio);
+    }
+
     public double getEncoderRotations() {
-        return climberMotor.getEncoder().getPosition();
+        return climberEncoder.getPosition();
     }
 }
