efe6b60c4d0e23a6a2d7e0f4e12d01ecd0ac5390
Floyd Moore <floyd.moore@gmail.com>
2024-05-22 13:21:01 -0700

    working on shooting command

---
+++ src/main/java/frc/robot/commands/SpinnupShooter.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class SpinnupShooter extends Command {
+  private ShooterSubsystem m_shooter;
+
+  /** Creates a new SpinupShooter. */
+  public SpinnupShooter(ShooterSubsystem shooter) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_shooter = shooter;
+
+    addRequirements(m_shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("Start SpinnupShooter Command");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_shooter.setLeftMotorVoltage(Constants.ShooterConstants.shooterSpeed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    System.out.println("End SpinnupShoot command");
+    m_shooter.setLeftMotorVoltage(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
