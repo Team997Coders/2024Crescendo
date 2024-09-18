291d7c678795a4e677a0252334e03a022088331e
Floyd Moore <floyd.moore@gmail.com>
2024-09-18 11:45:46 -0700

    removing unused import

--- src/main/java/frc/robot/commands/indexAndShootCommand/Index.java
+++ src/main/java/frc/robot/commands/indexAndShootCommand/Index.java
@@ -1,7 +1,6 @@
 package frc.robot.commands.indexAndShootCommand;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.IndexerSubsystem;
 
 public class Index extends Command {
