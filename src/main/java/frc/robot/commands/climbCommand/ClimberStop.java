3b4d6782fd1c0c7061a0bb4e0bbbcd35d7751d10
Floyd Moore <floyd.moore@gmail.com>
2024-10-01 13:50:27 -0700

    changes...

--- src/main/java/frc/robot/commands/climbCommand/ClimberStop.java
+++ src/main/java/frc/robot/commands/climbCommand/ClimberStop.java
@@ -1,31 +1,33 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 package frc.robot.commands.climbCommand;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimberSubsystem;
+
 public class ClimberStop extends Command {
     private ClimberSubsystem climber;
+
     public ClimberStop(ClimberSubsystem climber) {
         this.climber = climber;
         addRequirements(climber);
     }
+
     @Override
-    public void initialize() {   
+    public void initialize() {
     }
+
     @Override
     public void execute() {
         climber.setMotorVoltage(0);
     }
+
     @Override
     public void end(boolean interrupted) {
         climber.setMotorVoltage(0);
     }
-    @Override 
+
+    @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
-
-
 
 }
