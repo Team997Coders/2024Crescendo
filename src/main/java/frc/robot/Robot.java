10de5575d5a4d22d49b040554852e15b7779051e
Floyd Moore <floyd.moore@gmail.com>
2024-10-09 15:07:09 -0700

    testing the color sensor code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,10 @@ package frc.robot;
 import com.pathplanner.lib.commands.FollowPathCommand;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import lib.APDS9960;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,6 +26,8 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private APDS9960 apds;
+
   /**
    * This function is run when the robot is first started up and should be used
    * for any
@@ -33,6 +37,8 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     FollowPathCommand.warmupCommand().schedule();
 
+    apds = new APDS9960();
+    
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our
     // autonomous chooser on the dashboard.
@@ -93,6 +99,13 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
+    Color color = apds.readColor();
+    System.out.println("Color: R="+ color.red + " G=" + color.green + " B=" + color.blue);
+
+    int proximity = apds.readProximity();
+    System.out.println("Proximity: " + proximity);
+
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
